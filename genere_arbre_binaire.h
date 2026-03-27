6529e686562bd42466b10ab197bd609dd9d668a3
mams771 <122211215+mams771@users.noreply.github.com>
2026-03-27 18:57:53 +0100

    Update genere_arbre_binaire.h with new function declarations
    Added function declarations for tree traversal and node counting.

--- genere_arbre_binaire.h
+++ genere_arbre_binaire.h
@@ -1,4 +1,4 @@
-/* genere arbre */
+/* genere_arbre_binaire.h */
 
 #ifndef GENERE_ARBRE_BINAIRE_H
 #define GENERE_ARBRE_BINAIRE_H
@@ -8,5 +8,12 @@
 
 /* construit un arbre binaire de taille n à partir de son codage*/
 int construit_quelconque(Arbre * a, int ** codage, int n);
+
+/* Renvoie le nombre de noeud dans le sous abre gauche à partir de son nombre 
+de noeuds total */
+int nb_noeuds_gauche(int n);
+
+/* Renvoie un tableau du parcours prefixe d'un arbre à partir de son parcours infixe */
+void parcours_infixe_2_prefixe_presque_complet(int *prefixe, int *infixe, int n)
 
 #endif
