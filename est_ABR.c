69c60915e0001f44301f8a96c04e66290d127a46
mams771 <122211215+mams771@users.noreply.github.com>
2026-03-29 14:17:31 +0200

    Add files via upload
    FICHIER A JOURS SAUF GENERER FILIFORME A CHANGE

--- est_ABR.c
+++ est_ABR.c
@@ -4,13 +4,13 @@
 #include "est_ABR.h"
 #include "genere_arbre_binaire.h"
 
-int abr_min(Arbre a){
+static int abr_min(Arbre a){
     while (a->fg != NULL)
         a = a->fg;
     return a->valeur;
 }
 
-int abr_max(Arbre a){
+static int abr_max(Arbre a){
     while (a->fd != NULL)
         a = a->fd;
     return a->valeur;
@@ -44,59 +44,36 @@ int est_abr_infixe(Arbre a, long long *nb_visites) {
     *nb_visites = 0;
     return infixe_croissant(a, &dernier_noeud, nb_visites);
 }
-
+//definition
 int est_abr_definition_aux(Arbre a, int *min, int *max, long long *nb_visites){
     if (a == NULL)
         return 1;
+
     (*nb_visites)++;
+
     int minG, maxG, minD, maxD;
-    //sous arbre gauche
+
     if (a->fg != NULL){
         if (!est_abr_definition_aux(a->fg, &minG, &maxG, nb_visites))
             return 0;
         if (maxG > a->valeur)
             return 0;
     }
-    //sous arbre droit
+
     if (a->fd != NULL){
         if (!est_abr_definition_aux(a->fd, &minD, &maxD, nb_visites))
             return 0;
         if (minD < a->valeur)
             return 0;
     }
-    if (a->fg != NULL)
-        *min = minG;
-    else
-        *min = a->valeur;
-    if (a->fd != NULL)
-        *max = maxD;
-    else
-        *max = a->valeur;
+
+    *min = (a->fg != NULL) ? minG : a->valeur;
+    *max = (a->fd != NULL) ? maxD : a->valeur;
+
     return 1;
 }
 
-int infixe_croissant(Arbre a, Noeud **dernier_noeud) {
-    if (a == NULL)
-        return 1;
-    if (!infixe_croissant(a->fg, dernier_noeud))
-        return 0;
-    if (*dernier_noeud != NULL && (*dernier_noeud)->valeur >= a->valeur)
-        return 0;
-    *dernier_noeud = a;
-    return infixe_croissant(a->fd, dernier_noeud);
-}
-
-int est_abr_infixe(Arbre a) {
-    Noeud *dernier_noeud = NULL;
-    return infixe_croissant(a, &dernier_noeud);
-}
-
-int main(){
-    long long nb_visites = 0;
-    Arbre a = alloue_noeud(5);
-    a->fd = alloue_noeud(6);
-    a->fd->fg = alloue_noeud(3);
-    a->fg = alloue_noeud(4);
-    printf("est abr : %d \nnb visites : %lld \n", est_abr_naif(a, &nb_visites), nb_visites);
-    return 0;
+int est_abr_definition(Arbre a, long long *nb_visites){
+    int min, max;
+    return est_abr_definition_aux(a, &min, &max, nb_visites);
 }
--- genere_arbre_binaire.h
+++ genere_arbre_binaire.h
@@ -3,29 +3,45 @@
 #ifndef GENERE_ARBRE_BINAIRE_H
 #define GENERE_ARBRE_BINAIRE_H
 
-#include "est_ABR_V2.h"
+#include "est_ABR.h"
 
-/* Alloue de l'espace en memoire pour un arbre et lui assigne val en etiquette */
+/* Alloue un noeud avec val comme etiquette */
 Arbre alloue_noeud(int val);
 
-/* Libere l'espace memoire alloue */
+/* Libere l'espace memoire de l'arbre */
 void libere_arbre(Arbre a);
 
-/* construit un arbre binaire de taille n à partir de son codage*/
-int construit_quelconque(Arbre * a, int ** codage, int n);
+/* Construit un arbre binaire a partir de son codage prefixe avec sentinelles -1 */
+int construit_quelconque(Arbre *a, int **codage, int n);
 
-/* Renvoie le nombre de noeud dans le sous abre gauche à partir de son nombre 
-de noeuds total */
+/* Renvoie le nombre de noeuds dans le sous-arbre gauche d'un arbre presque complet a n noeuds */
 int nb_noeuds_gauche(int n);
 
-/* Renvoie un tableau du parcours prefixe d'un arbre à partir de son parcours infixe */
+/* Transforme un parcours infixe en prefixe pour un arbre presque complet */
 void parcours_infixe_2_prefixe_presque_complet(int *prefixe, int *infixe, int n);
 
+/* Transforme un parcours infixe en prefixe pour un arbre filiforme choisi aleatoirement */
+void parcours_infixe_2_prefixe_filiforme_aleatoire(int *prefixe, int *infixe, int n);
 
-/* Crée aléatoirement un arbre binaire de recherche presque complet */
-int ABR_presque_complet_alea(Arbre * a, int taille);
+/* Transforme un parcours infixe en codage prefixe pour un arbre quelconque choisi aleatoirement */
+void parcours_infixe_2_prefixe_quelconque_aleatoire(int *codage, int *infixe, int n);
 
-/*  Crée aléatoirement un arbre presque complet */
-int non_ABR_presque_complet_alea(Arbre * a, int taille);
+/* Cree aleatoirement un ABR presque complet de taille noeuds */
+int ABR_presque_complet_alea(Arbre *a, int taille);
+
+/* Cree aleatoirement un arbre presque complet non-ABR de taille noeuds */
+int non_ABR_presque_complet_alea(Arbre *a, int taille);
+
+/* Cree aleatoirement un ABR de forme quelconque de taille noeuds */
+int ABR_quelconque_alea(Arbre *a, int taille);
+
+/* Cree aleatoirement un arbre quelconque non-ABR de taille noeuds */
+int non_ABR_quelconque_alea(Arbre *a, int taille);
+
+/* Cree aleatoirement un ABR filiforme de taille noeuds */
+int ABR_filiforme_alea(Arbre *a, int taille);
+
+/* Cree aleatoirement un arbre filiforme non-ABR de taille noeuds */
+int non_ABR_filiforme_alea(Arbre *a, int taille);
 
 #endif
